03e47d3515c560eff13fbb8302a20d4b25195fbd
Siddeshwar <siddeshwar.work@gmail.com>
2024-05-09 15:47:20 +0530

    connecting with db using gorm, rendering it to template

--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func main() {
-	router := gin.Default()
+type Dish struct {
+	gorm.Model
+	ID          uint
+	Name        string
+	Description string
+	Thumbnail   string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
 
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Dish{})
+
+	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", func(c *gin.Context) {
+		var dishes []Dish
+		db.Find(&dishes)
+
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"Name": "Siddeshwar",
+            "Dishes": dishes,
 		})
 	})
 
