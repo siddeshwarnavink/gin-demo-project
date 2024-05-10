9d131597d5023306fb8fa4d917299803c6b45e76
Siddeshwar <siddeshwar.work@gmail.com>
2024-05-10 07:42:32 +0530

    quick typo fix

--- main.go
+++ main.go
@@ -15,19 +15,19 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	recipie := new(controllers.RecipieController)
+	recipieController := new(controllers.RecipieController)
 	createGroup := router.Group("/create")
 	{
-		createGroup.GET("", recipie.GetCreate)
-		createGroup.POST("", recipie.PostCreate)
+		createGroup.GET("", recipieController.GetCreate)
+		createGroup.POST("", recipieController.PostCreate)
 	}
 	editGroup := router.Group("/edit/:id")
 	{
-		editGroup.GET("", recipie.GetEdit)
-		editGroup.POST("", recipie.PostEdit)
+		editGroup.GET("", recipieController.GetEdit)
+		editGroup.POST("", recipieController.PostEdit)
 	}
-	router.GET("/delete/:id", recipie.Delete)
-	router.GET("/", recipie.Home)
+	router.GET("/delete/:id", recipieController.Delete)
+	router.GET("/", recipieController.Home)
 
 	router.Run()
 }
